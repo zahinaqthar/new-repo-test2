0daf19e8d514fa3207e1a2da02e1e3c903a1f940
zahinaqthar <achmad.20041@mhs.unesa.ac.id>
2022-10-11 15:42:25 +0700

    Menambah list baru

--- index.php
+++ index.php
@@ -9,5 +9,10 @@
 <body>
     <p>Hello World</p>
     <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Aperiam quos officia magnam corrupti veniam odit optio maxime quam quisquam culpa fuga nulla tempore ex repellat facilis odio, provident iusto sint!</p>
+    <ul>
+        <li>satu</li>
+        <li>dua</li>
+        <li>tiga</li>
+    </ul>
 </body>
 </html>
