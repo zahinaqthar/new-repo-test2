8373b67b28e79bdadf46d95ac5b44d46090efc85
zahinaqthar <achmad.20041@mhs.unesa.ac.id>
2022-10-11 15:35:52 +0700

    Menambah paragraf baru

---
+++ index.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Test New Repo Remote</title>
+</head>
+<body>
+    <p>Hello World</p>
+    <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Aperiam quos officia magnam corrupti veniam odit optio maxime quam quisquam culpa fuga nulla tempore ex repellat facilis odio, provident iusto sint!</p>
+</body>
+</html>
